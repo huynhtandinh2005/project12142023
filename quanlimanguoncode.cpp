d5ed396c186c67c9a462eb2d50001b9936597ff5
huynhtandinh2005 <huynhdinh2k52707@gmail.com>
2023-12-14 18:54:34 +0700

    Dinh

---
+++ quanlimanguoncode.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello";
+	char per1[50]="1.Huynh Tan Dinh";
+	char per2[50];
+	cout<<"Quan li ma nguon: "<<endl;
+	cout<<per1<<endl;
+	cout<<per2<<endl;
+}
